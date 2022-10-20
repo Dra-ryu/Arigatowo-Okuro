eab45b8d0bb902e5cac37c02183a6bbaa88d32ac
Dra-ryu <ryunosuke.harada25@gmail.com>
2022-10-20 18:24:10 +0900

    可読性の修正

--- timer.py
+++ timer.py
@@ -1,13 +1,8 @@
 import json
-import os
-import requests
-import sys
 import sqlite3
-
-from flask import Flask, render_template, request, abort, redirect
+from flask import Flask, render_template, request, redirect
 from linebot import LineBotApi
 from linebot.models import TextSendMessage
-from flask_session import Session
 
 
 db = sqlite3.connect("thank.db", check_same_thread=False)
@@ -15,16 +10,13 @@ db = sqlite3.connect("thank.db", check_same_thread=False)
 app = Flask(__name__)
 
 # 以下、LINE bot関連の情報
-
 #  チャネルアクセストークンはhttps://developers.line.biz/console/channel/1656501143/messaging-apiから取得
 LINE_CHANNEL_ACCESS_TOKEN = "gJvukELuaCOxRtd0jkucrdgIU15Cvf421lQWjRZv6+08RhE97ZPtbdwQUZ8S/JMBU3X+cTM8afrFoKdpHNGjo7EmXoO8Qs3IYh+87PGU6vM1YK5D7QF+FE5VM6ko73gk7dbMb+yUCVPcb+edMFhq+QdB04t89/1O/w1cDnyilFU="
 
 line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
 
 
-# タイマー機能(Ryu)
 def timer():
-
     return render_template("time.html")
 
 
@@ -42,26 +34,22 @@ def message_send(username):
     # ポイント計算
     db.execute("SELECT point FROM users WHERE name = ?", (username, ))
     old_point = db.fetchall()[0][0]
-    print("ここまでのポイント", old_point, username)
+
     new_point = old_point + elapsedTime
+
+    #  ポイント更新
     db.execute("UPDATE users SET point = ? WHERE name = ?", (new_point, username))
     conn.commit()
 
-
-    # [0]['name']をつけないと、[{'name': 'Ryu test'}, {'name': 'aaaa'}]という感じで全ての値が出てきちゃう
-    # そのため、1つ目のデータのnameに格納されている値を取ってくるとGood!
-
-    db.execute("SELECT id FROM users WHERE name = ?", (partner_username,))  # javascriptはクライアントサイド→sqliteを普通には動かせない→pythonでやることに
+    #  通知を送る友人のidを取得
+    db.execute("SELECT id FROM users WHERE name = ?", (partner_username,))
     partner_user_id = db.fetchall()[0][0]
-    print(partner_user_id, elapsedTime, housework_name)
 
+    #  タイマー計測終了後にLINEメッセージ生成と送信
     messages = TextSendMessage(text=f"{username}さんが{housework_name}を{elapsedTime}分しました！\n\n"
                                     f"ありがとうを送りましょう☺\n\n"
                                     f"https://presentthanks.pythonanywhere.com/point") 
 
     line_bot_api.push_message(partner_user_id, messages)
+
     return redirect('/home') 
-
-
-if __name__ == "__main__":
-    app.run()
