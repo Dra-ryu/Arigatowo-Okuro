8cda742bee37226957ff97324423bf80915a4344
Dra-ryu <ryunosuke.harada25@gmail.com>
2022-11-03 16:20:00 +0900

    カーソルの変数名変更

--- timer.py
+++ timer.py
@@ -4,13 +4,9 @@ from flask import Flask, render_template, request, redirect
 from linebot import LineBotApi
 from linebot.models import TextSendMessage
 
-
-db = sqlite3.connect("thank.db", check_same_thread=False)
-
 app = Flask(__name__)
 
-# 以下、LINE bot関連の情報
-#  チャネルアクセストークンはhttps://developers.line.biz/console/channel/1656501143/messaging-apiから取得
+# セキュリティ上の問題のため、利用時はLINE DEVELOPERSから値を代入する
 LINE_CHANNEL_ACCESS_TOKEN = "チャネルアクセストークン"
 
 line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
@@ -20,36 +16,37 @@ def timer():
     return render_template("time.html")
 
 
+#  家事時間計測終了後の処理
 def message_send(username):
+    conn = sqlite3.connect("thank.db")
+    cur = conn.cursor()
 
-    conn = sqlite3.connect("thank.db", check_same_thread=False)
-    db = conn.cursor()
-
-    # Javascriptからデータを受け取り、辞書から各データを取り出す
+    # time.jsからJSONを受け取り、データを取り出す
     data = request.get_json()
     elapsedTime = data['elapsedTime']
     housework_name = data['housework_name']
     partner_username = data['partner_username']
 
-    # ポイント計算
-    db.execute("SELECT point FROM users WHERE name = ?", (username, ))
-    old_point = db.fetchall()[0][0]
+    # ポイントの加算と更新
+    cur.execute("SELECT point FROM users WHERE name = ?", (username, ))
+    old_point = cur.fetchall()[0][0]
 
+    #  1分ごとに1ポイント加算される
     new_point = old_point + elapsedTime
 
-    #  ポイント更新
-    db.execute("UPDATE users SET point = ? WHERE name = ?", (new_point, username))
+    cur.execute("UPDATE users SET point = ? WHERE name = ?", (new_point, username))
     conn.commit()
 
-    #  通知を送る友人のidを取得
-    db.execute("SELECT id FROM users WHERE name = ?", (partner_username,))
-    partner_user_id = db.fetchall()[0][0]
+    #  通知を送るパートナーのidを取得
+    cur.execute("SELECT id FROM users WHERE name = ?", (partner_username,))
+    partner_user_id = cur.fetchall()[0][0]
 
-    #  タイマー計測終了後にLINEメッセージ生成と送信
+    #  LINEメッセージ生成と送信
     messages = TextSendMessage(text=f"{username}さんが{housework_name}を{elapsedTime}分しました！\n\n"
                                     f"ありがとうを送りましょう☺\n\n"
                                     f"https://presentthanks.pythonanywhere.com/point") 
 
     line_bot_api.push_message(partner_user_id, messages)
 
+    conn.close()
     return redirect('/home') 
