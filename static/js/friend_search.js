8f5b471b65ec8f1789bdff24bacee4a30031fcac
Dra-ryu <ryunosuke.harada25@gmail.com>
2022-11-04 09:26:19 +0900

    コメントアウト修正

--- static/js/friend_search.js
+++ static/js/friend_search.js
@@ -1,28 +1,26 @@
 'use strict';
-
 {
     // 定数を定義して、HTMLから要素を取得
     const add = document.getElementById('add');
 
     // 追加のボタンが押された時の処理
     add.addEventListener('click', () => {
+      // プルダウンの値を取得
+      const added_friend_name = document.getElementById('friend_add').value;
 
-    // セレクトボックスの値を取得
-    const added_friend_name = document.getElementById('friend_add').value;
+      // /addのルートに値を送る
+      $.ajax({
+        type: "POST",
+        contentType: "application/json; charset=utf-8",
+        url: "/add",
+        data: JSON.stringify(added_friend_name),
+        dataType: "json"
+      });
 
-    // /addのルートに値を送る
-    $.ajax({
-      type: "POST",
-      contentType: "application/json; charset=utf-8",
-      url: "/add",
-      data: JSON.stringify(added_friend_name),// JSON文字列への変換をして、辞書をpythonに送る
-      dataType: "json"
-    });
-
-    // 1マイクロ秒後に画面遷移→タイマー機能へ
-    setTimeout(function(){
-      window.location.href = 'https://presentthanks.pythonanywhere.com/friend';
-    }, 100);
+      // 画面遷移
+      setTimeout(function(){
+        window.location.href = 'https://presentthanks.pythonanywhere.com/friend';
+      }, 100);
 
     });
 
