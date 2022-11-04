56f6177fd44e1e13b1ca1855517d4d3332bac48d
Dra-ryu <ryunosuke.harada25@gmail.com>
2022-11-04 09:26:31 +0900

    URLを修正

--- static/js/home.js
+++ static/js/home.js
@@ -1,5 +1,4 @@
 'use strict';
-
 {
     // 定数を定義して、HTMLから要素を取得
     const cooking = document.getElementById('cooking');
@@ -89,84 +88,80 @@
 
 
     // 料理ボタンが押された時の処理
-    // 他のボタンが押されてinactiveになっていたら、反応しない
     cooking.addEventListener('click', () => {
-    if (cooking.classList.contains('inactive') === true) {
-      return;
-    }
+      if (cooking.classList.contains('inactive') === true) {
+        return;
+      }
 
-    // houseworkというkeyにcookingを入れる
-    // 家事名を入れることで、そのままtimerの表示でも使えるように改良(代入とかが必要ない)
-    localStorage.setItem('housework', '料理');
-
-    // cookingボタンが押された状態にする
-    cookingOn();
+      // 選択した家事名を次のタイマー画面で表示するためにローカルストレージに
+      localStorage.setItem('housework', '料理');
+      cookingOn();
     });
 
 
     // 買い物ボタンが押された時
     shopping.addEventListener('click', () => {
-    if (shopping.classList.contains('inactive') === true) {
-      return;
-    }
+      if (shopping.classList.contains('inactive') === true) {
+        return;
+      }
 
-    localStorage.setItem('housework', '買い物');
-    shoppingOn();
+      localStorage.setItem('housework', '買い物');
+      shoppingOn();
     });
 
 
     // 掃除ボタンが押された時
     cleaning.addEventListener('click', () => {
-    if (cleaning.classList.contains('inactive') === true) {
-      return;
-    }
+      if (cleaning.classList.contains('inactive') === true) {
+        return;
+      }
 
-    localStorage.setItem('housework', '掃除');
-    cleaningOn();
+      localStorage.setItem('housework', '掃除');
+      cleaningOn();
     });
 
 
     // 洗濯ボタンが押された時
     washing.addEventListener('click', () => {
-    if (washing.classList.contains('inactive') === true) {
-      return;
-    }
+      if (washing.classList.contains('inactive') === true) {
+        return;
+      }
 
-    localStorage.setItem('housework', '洗濯');
-    washingOn();
+      localStorage.setItem('housework', '洗濯');
+      washingOn();
     });
 
 
     // ゴミ捨てボタンが押された時
     garbage.addEventListener('click', () => {
-    if (garbage.classList.contains('inactive') === true) {
-      return;
-    }
+      if (garbage.classList.contains('inactive') === true) {
+        return;
+      }
 
-    localStorage.setItem('housework', 'ゴミ捨て');
-    garbageOn();
+      localStorage.setItem('housework', 'ゴミ捨て');
+      garbageOn();
     });
 
 
     // 送迎ボタンが押された時
     transfer.addEventListener('click', () => {
-    if (transfer.classList.contains('inactive') === true) {
-      return;
-    }
+      if (transfer.classList.contains('inactive') === true) {
+        return;
+      }
 
-    localStorage.setItem('housework', '送迎');
-    transferOn();
+      localStorage.setItem('housework', '送迎');
+      transferOn();
     });
 
 
     // その他の家事ボタンが押された時
     others.addEventListener('click', () => {
-    if (others.classList.contains('inactive') === true) {
-      return;
-    }
+      if (others.classList.contains('inactive') === true) {
+        return;
+      }
 
-    localStorage.setItem('housework', 'その他の家事');
-    othersOn();
+      localStorage.setItem('housework', 'その他の家事');
+      othersOn();
     });
 
 
@@ -174,26 +169,26 @@
     // 決定のボタンが押された時の処理
     decision.addEventListener('click', () => {
 
-    // 家事のボタンが押されていない時には決定を押せなくする
-    if (decision.classList.contains('inactive') === true) {
-      return;
-    }
+      // 家事のボタンが押されていない時には決定を押せなくする
+      if (decision.classList.contains('inactive') === true) {
+        return;
+      }
 
-    // 送る相手の選択と名前の取得
-    const friend_name = document.getElementById('friend_name').value;
+      // 送る相手の選択と名前の取得
+      const friend_name = document.getElementById('friend_name').value;
 
-    // 送る相手が選択されていなかった場合、決定を押せなくする
-    if (friend_name === '選択してください') {
-      return;
-    }
+      // 送る相手が選択されていなかった場合、決定を押せなくする
+      if (friend_name === '選択してください') {
+        return;
+      }
 
-    // 選択されたfriend_nameをローカルストレージへ
-    localStorage.setItem('friend_name', friend_name);
+      // 選択されたfriend_nameをローカルストレージへ
+      localStorage.setItem('friend_name', friend_name);
 
-    // 100マイクロ秒後に画面遷移→タイマー機能へ
-    setTimeout(function(){
-      window.location.href = 'https://presentthanks.pythonanywhere.com//timer';
-    }, 100);
+      // タイマー機能へ画面遷移
+      setTimeout(function(){
+        window.location.href = 'https:/presentthanks.pythonanywhere.com/timer';
+      }, 100);
 
   });
 }
